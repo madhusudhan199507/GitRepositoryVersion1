9f9bb59ef3de3a6d30249eac07a3d35c09b5ced8
dhfl <dhfl@DESKTOP-6MVRPSK>
2021-01-04 10:19:21 +0530

    first Commit...

---
+++ src/Test1.java
@@ -0,0 +1,11 @@
+
+public class Test1 {
+	
+	public static void main(String[] args) {
+		int a =10;
+		int b = 20;
+		int total = a+b;
+		System.out.println("total count is :::: " +total );
+	}
+
+}
